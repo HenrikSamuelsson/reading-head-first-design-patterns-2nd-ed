37e0b43ffa539318267c06ccd2ed100d2be638b8
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2025-01-04 09:08:00 +0100

    Add a model duck.

--- ducks/Duck.java
+++ ducks/Duck.java
@@ -18,4 +18,12 @@ public abstract class Duck {
     public void swim() {
         System.out.println("All ducks float, even decoys!");
     }
+
+    public void setFlyBehaviour(FlyBehaviour fb) {
+        flyBehaviour = fb;
+    }
+
+    public void setQuackBehaviour(QuackBehaviour qb) {
+        quackBehaviour = qb;
+    }
 }
